91834965826a173f6b353cf35cd06446dfb67f08
garapatirakesh <41864888+garapatirakesh@users.noreply.github.com>
2022-02-23 20:31:35 +0530

    Update p_util_new.py

--- p_util_new.py
+++ p_util_new.py
@@ -54,3 +54,89 @@ def get_ad_group_names(ad_groups, headers):
                 print('Failed at get_ad_group_names ', x.__class__.__name__)
                 sleep(30)
                 print(f"Retrying {ad_group}")
+                
+
+
+                
+                
+                
+def ad_groups_to_df(ad_groups):
+    ad_group_ids = []
+    ad_group_names = []
+    campaign_ids = []
+    campaign_names = []
+    dates = []
+    spends = []
+    clicks = []
+    impressions = []
+    publishers = []
+
+    for ad_group in ad_groups:
+        length = len(ad_groups[ad_group]['date_start'])
+        print(ad_group)
+        print(length)
+        for i in range(length):
+            if 'campaign_id' in ad_groups[ad_group]:
+                print("yes")
+                campaign_id = ad_groups[ad_group]['campaign_id']
+            else:
+                print(f"AD GROUP {ad_group} IS MISSING campaign_id")
+                continue
+
+            if 'campaign_name' in ad_groups[ad_group]:
+                campaign_name = ad_groups[ad_group]['campaign_name']
+            else:
+                print(f"AD GROUP {ad_group} IS MISSING campaign_name")
+                continue
+
+            if 'name' in ad_groups[ad_group]:
+                ad_group_name = ad_groups[ad_group]['name']
+            else:
+                print(f"AD GROUP {ad_group} IS MISSING name")
+                continue
+
+            if 'date_start' in ad_groups[ad_group]:
+                date = ad_groups[ad_group]['date_start'][i]
+            else:
+                print(f"AD GROUP {ad_group} IS MISSING dates")
+                continue
+
+            if 'spend' in ad_groups[ad_group]:
+                spend = ad_groups[ad_group]['spend'][i]
+            else:
+                print(f"AD GROUP {ad_group} IS MISSING spend")
+                continue
+
+            if 'clicks' in ad_groups[ad_group]:
+                click = ad_groups[ad_group]['clicks'][i]
+            else:
+                print(f"AD GROUP {ad_group} IS MISSING clicks")
+                continue
+
+            if 'impressions' in ad_groups[ad_group]:
+                impression = ad_groups[ad_group]['impressions'][i]
+            else:
+                print(f"AD GROUP {ad_group} IS MISSING impressions")
+                continue
+            print(campaign_id,campaign_name,ad_group_name,date)
+            publisher = "Pinterest"
+
+            campaign_ids.append(campaign_id)
+            campaign_names.append(campaign_name)
+            ad_group_ids.append(ad_group)
+            ad_group_names.append(ad_group_name)
+            dates.append(date)
+            spends.append(spend)
+            clicks.append(click)
+            impressions.append(impression)
+            publishers.append(publisher)
+
+    return pd.DataFrame({'ad_group_id': ad_group_ids,
+                         'ad_group_name': ad_group_names,
+                         'campaign_id': campaign_ids,
+                         'campaign_name': campaign_names,
+                         'date_start': dates,
+                         'spend': spends,
+                         'clicks': clicks,
+                         'impressions': impressions,
+                         'publisher': publishers})
